400413d741f2468d309f279ec78c9f0150c1fd4c
Carlos Fernandez <cafl.23w@gmail.com>
2022-10-09 23:58:31 +0200

    xd:zero

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-import './App.css';
+import React from "react";
+import PocketBase from "pocketbase";
+import { useState, useEffect } from "react";
+
+const client = new PocketBase("http://127.0.0.1:8090");
 
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
+  const [cards, setCards] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    async function fetchCards() {
+      const result = await client.records.getFullList("cards");
+      console.log(result);
+      setCards(result);
+    }
+    fetchCards();
+    setIsLoading(false);
+  }, []);
+
+  return isLoading ? (
+    <p>Loading</p>
+  ) : (
+    cards.map((element) => <div>{JSON.stringify(element)}</div>)
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
-  <p>hey</p>
   </React.StrictMode>
 );
 
