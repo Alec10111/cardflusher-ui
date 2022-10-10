3441fa1fb3c312b3e284bdea1647a2abaa4c549a
Carlos Fernandez <cafl.23w@gmail.com>
2022-10-10 18:48:36 +0200

    xd

--- src/App.js
+++ src/App.js
@@ -1,28 +1,9 @@
 import React from "react";
 import PocketBase from "pocketbase";
-import { useState, useEffect } from "react";
-
-const client = new PocketBase("http://127.0.0.1:8090");
+import LoginPage from "./pages/LoginPage/LoginPage";
 
 function App() {
-  const [cards, setCards] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    async function fetchCards() {
-      const result = await client.records.getFullList("cards");
-      console.log(result);
-      setCards(result);
-    }
-    fetchCards();
-    setIsLoading(false);
-  }, []);
-
-  return isLoading ? (
-    <p>Loading</p>
-  ) : (
-    cards.map((element) => <div>{JSON.stringify(element)}</div>)
-  );
+  return <LoginPage/>
 }
 
 export default App;
---
+++ src/pages/LoginPage/LoginPage.js
@@ -0,0 +1,57 @@
+import React, { useState } from 'react';
+import PocketBase from 'pocketbase';
+
+const client = new PocketBase('http://127.0.0.1:8090');
+
+// const userAuthData1 = await client.users.authViaEmail('test@example.com', '123456');
+
+
+function login()
+
+
+function LoginPage(props) {
+
+  const [userDetails, setuserDetails] = useState({
+      email: "",
+      password: ""
+    });
+  function handleChange(event) {
+    const { value, name } = event.target;
+
+    setuserDetails(prevValue => {
+      if (name === "email") {
+        return {
+          email: value,
+          passowrd: prevValue.password
+        };
+      } else if (name === "password") {
+        return {
+          email: prevValue.email,
+          password: value
+        };
+      }
+    });
+  }
+  
+  return  <div>
+    <h2>Login</h2>
+     <form>
+      <input
+          name="email"
+          onChange={handleChange}
+          placeholder="Email"
+          defaultValue={userDetails.email}
+        />
+        <input
+          name="password"
+          onChange={handleChange}
+          placeholder="Password"
+          type="password"
+          defaultValue={userDetails.password}
+        />
+  <button>LOGIN</button>
+   </form>
+    </div>
+}
+
+export default LoginPage;
