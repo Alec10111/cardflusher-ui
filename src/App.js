45088455603b591f87aae96a597cc772811f0873
Carlos Fernandez <cafl.23w@gmail.com>
2022-10-10 19:58:32 +0200

    removed nextui componets

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React from "react";
 import PocketBase from "pocketbase";
 import LoginPage from "./pages/LoginPage/LoginPage";
-import { NextUIProvider } from '@nextui-org/react';
 
 function App() {
   return (
-    <NextUIProvider>
       <LoginPage/>
-    </NextUIProvider>
   );
 }
 
